f74a24aaa10f5aee894c7e66ae9583c938e5e7ad
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-05-28 01:03:10 +0200

    creating badwords model

--- posts/models.py
+++ posts/models.py
@@ -7,13 +7,12 @@ from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
-    name = models.CharField(max_length = 30)
-    subscribers = models.ManyToManyField(User, blank=True, related_name="subscribers")
-
+    name = models.CharField(max_length=30)
+    subscribers = models.ManyToManyField(
+        User, blank=True, related_name="subscribers")
 
     def __str__(self):
         return self.name
-
 
 
 # class Subscribe(models.Model):
@@ -21,17 +20,17 @@ class Category(models.Model):
 #     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
-
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length = 50)
-    content = models.TextField(max_length = 600)
-    image = models.ImageField(upload_to = "posts")
-    publish = models.DateField(auto_now_add = True)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=600)
+    image = models.ImageField(upload_to="posts")
+    publish = models.DateField(auto_now_add=True)
     categories = models.ManyToManyField(Category)
     slug = models.SlugField(primary_key=True, db_index=True)
     likes = models.ManyToManyField(User, related_name="likes", blank=True)
-    dislikes = models.ManyToManyField(User, related_name="dislikes", blank=True)
+    dislikes = models.ManyToManyField(
+        User, related_name="dislikes", blank=True)
     # tags
 
     def save(self, **kwargs):
@@ -41,9 +40,15 @@ class Post(models.Model):
     def like_count(self):
         return self.likes.count()
 
-
     # class Meta:
     #     ordering = ['publish']
 
     def __str__(self):
         return self.title
+
+
+class Badwords(models.Model):
+    badword = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.badword
