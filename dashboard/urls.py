d091eb2901ba40c6ff4b63803ac0a43f789fc9fa
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-05-28 01:00:27 +0200

    updating badwords urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-
-from .views import *
+from django.urls.conf import include
+from dashboard import views
 
 app_name = "dashboard"
 
 urlpatterns = [
-    path('auth/home', home, name="home"),
+    path('auth/home', views.home),
+    # Badwords urls
+    path("auth/badwords", views.all_badwords),
+    path("auth/add/badword", views.add_badwords),
+    path("auth/badword_details/<badword_id>", views.badwords_details),
+    path("auth/badword/edit/<badword_id>", views.edit_badwords),
+    path("auth/badword/delete/<badword_id>", views.delete_badwords)
 ]
