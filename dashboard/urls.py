1dd72bf0cb12e841de4a120ea427b8f0d2073fa3
aya-maher <ayamaherali9@gmail.com>
2021-05-31 01:05:07 +0200

    urls for categories

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,5 +11,13 @@ urlpatterns = [
     path("auth/add/badword", views.add_badwords),
     path("auth/badword_details/<badword_id>", views.badwords_details),
     path("auth/badword/edit/<badword_id>", views.edit_badwords),
-    path("auth/badword/delete/<badword_id>", views.delete_badwords)
+    path("auth/badword/delete/<badword_id>", views.delete_badwords),
+
+     # categories urls
+    path("auth/categories", views.all_categories),
+    path("auth/add/category", views.add_categories),
+    path("auth/category_details/<category_id>", views.categories_details),
+    path("auth/category/edit/<category_id>", views.edit_categories),
+    path("auth/category/delete/<category_id>", views.delete_categories)
+
 ]
