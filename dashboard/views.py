028b2c3e3f7d6cc69e3c30164813ac69b281de95
aya-maher <ayamaherali9@gmail.com>
2021-05-31 01:09:24 +0200

    views for crud categories

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, HttpResponseRedirect
-from dashboard.forms import BadwordsForm
+from dashboard.forms import BadwordsForm , CategoriesForm
 from posts.models import Badwords
-
+from posts.models import Category
 # Create your views here.
 
 
@@ -54,3 +54,48 @@ def delete_badwords(request, badword_id):
     badwords = Badwords.objects.get(id=badword_id)
     badwords.delete()
     return HttpResponseRedirect("/auth/badwords")
+
+
+# categories
+
+def all_categories(request):
+    categories = Category.objects.all()
+    context = {"data": categories}
+    return render(request, 'auth/dashboard/categories/categories.html', context)
+
+
+def add_categories(request):
+    categories_form = CategoriesForm()
+    if request.method == 'POST':
+        categories_form = CategoriesForm(request.POST)
+        if categories_form.is_valid():
+            categories_form.save()
+            return HttpResponseRedirect("/auth/categories")
+
+    context = {"categories_form": categories_form}
+    return render(request, 'auth/dashboard/categories/categories_form.html', context)
+
+
+def categories_details(request, category_id):
+    category = Category.objects.get(id=category_id)
+    context = {"data": category}
+    return render(request, 'auth/dashboard/categories/categories_details.html', context)
+
+
+def edit_categories(request, category_id):
+    categories = Category.objects.get(id=category_id)
+    categories_form = CategoriesForm(instance=categories)
+    if request.method == 'POST':
+        categories_form = CategoriesForm(request.POST, instance=categories)
+        if categories_form.is_valid():
+            categories_form.save()
+            return HttpResponseRedirect('/auth/categories')
+
+    context = {'categories_form': categories_form}
+    return render(request, 'auth/dashboard/categories/categories_form.html', context)    
+
+
+def delete_categories(request, category_id):
+    categories = Category.objects.get(id=category_id)
+    categories.delete()
+    return HttpResponseRedirect("/auth/categories")
