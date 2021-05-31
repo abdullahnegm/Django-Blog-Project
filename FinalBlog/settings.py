c2af1ff4a3adeba26468a9bc462323d72d0f22b4
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-06-01 01:47:50 +0200

    mirging aya branch with bermawy branch

--- FinalBlog/settings.py
+++ FinalBlog/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'FinalBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-blog',
+        'USER': 'admin',
+        'PASSWORD': 'admin'
     }
 }
 
