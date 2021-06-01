951a005a0cb1dd2032d08453ec37666377982f61
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-06-01 21:35:36 +0200

    adding users urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,9 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('auth/home', views.home),
+
+
+
     # Badwords urls
     path("auth/badwords", views.all_badwords),
     path("auth/add/badword", views.add_badwords),
@@ -13,11 +16,20 @@ urlpatterns = [
     path("auth/badword/edit/<badword_id>", views.edit_badwords),
     path("auth/badword/delete/<badword_id>", views.delete_badwords),
 
-     # categories urls
+    # categories urls
     path("auth/categories", views.all_categories),
     path("auth/add/category", views.add_categories),
     path("auth/category_details/<category_id>", views.categories_details),
     path("auth/category/edit/<category_id>", views.edit_categories),
-    path("auth/category/delete/<category_id>", views.delete_categories)
+    path("auth/category/delete/<category_id>", views.delete_categories),
+
+    # users urls
+    path("auth/users", views.all_users),
+    path("auth/add/user", views.add_user),
+    path("auth/user_details/<user_id>", views.user_details),
+    path("auth/user/block/<user_id>", views.block),
+    path("auth/user/unblock/<user_id>", views.un_block),
+    path("auth/user/promote/<user_id>", views.promote),
+    path("auth/user/delete/<user_id>", views.delete_user),
 
 ]
