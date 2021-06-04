7a61d8c82cc3ae596966881c8bc9863c76c24cc3
abdullahnegm <abdullahahmed1997@hotmail.com>
2021-06-04 19:04:35 +0200

    Post urls

--- posts/urls.py
+++ posts/urls.py
@@ -7,9 +7,6 @@ app_name = "posts"
 urlpatterns = [
     path('home', home, name="home"),
     path('<slug:slug>', detail, name="detail"),
-    path('like/<slug>/<is_liked>', like, name="like"),
-    path('subscribe/', like, name="like"),
-    path('search/', search, name="search"),
     path('create/', create, name="create"),
-    path('edit/<slug>', edit, name="edit"),
+    path('delete/<slug>', delete, name="delete"),
 ]
