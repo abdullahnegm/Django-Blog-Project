41316495e2571cd4328dc25cc6c423382cd67306
MuhmdGad <mohamedengineer944@gmail.com>
2021-06-05 15:36:06 +0200

    Fixing replay issue and modifing post design

--- comments/forms.py
+++ comments/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class commentForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea())
-
-    
+    content = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'w-100 form-control' , 'placeholder':'Say something...'})
+        )
